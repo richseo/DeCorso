9c02d3ccaf2cbfd22ea784f9094e1c25dc2b4d95
Delon Newman <delon.newman@gmail.com>
2011-12-13 00:41:04 -1000

    misc changes

--- site/app/helpers/application_helper.rb
+++ site/app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def truncate(text, length=20, sep='...')
+    if text.length > length
+      "#{text[0, length]}#{sep}"
+    else
+      text
+    end
+  end
+
+  def latest_news
+    raw(NewsItem.latest.map do |item|
+      "<div class=\"title\">#{item.title}</div>
+        <div class=\"publish-date\">#{item.publish_date.strftime("%B %d, %Y")}</div>
+        <div class=\"body\">#{truncate(item.body, 300)}</div>"
+    end.join('<br />'))
+  end
 end
